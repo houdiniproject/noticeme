10e3e5d4492c4dad8d64ecb97aadaaf6ed2122ac
Eric Schultz <eric@commitchange.com>
2020-07-06 16:56:35 -0500

    You can customize the NOTICE filename

--- cmd.js
+++ cmd.js
@@ -3,5 +3,9 @@
 // Licensed under the MIT License.
 const noticeme = require('.');
 const fs = require('fs');
-
-noticeme('.').then(notice => fs.writeFileSync('NOTICE', notice));
+const {argv} = require('yargs')
+let noticeFilename = "NOTICE"
+if (argv._.length >= 1) {
+  noticeFilename = argv._[0]
+}
+noticeme('.').then(notice => fs.writeFileSync(noticeFilename, notice));
