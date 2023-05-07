2a3a3345ca9639273c14417cb55935303d0f8fcc
Eric Schultz <eric@commitchange.com>
2023-05-07 12:19:00 -0500

    Convert to using rpt as a promise

--- noticeme.ts
+++ noticeme.ts
@@ -24,6 +24,19 @@ function retrieveIncludedJson(path: string): { name: string; version: string; }[
   return fs.existsSync(path) ? JSON.parse(fs.readFileSync(path, 'utf8')).packages : [];
 }
 
+function getPackages(rpt: typeof readPkgTree): (path: string) => Promise<PackageNode[]> {
+  return (path: string) => {
+    return new Promise((resolve, reject) => {
+      rpt(path, (err: any, { children }: { children: PackageNode[] }) => {
+        if (err)
+          reject(err)
+        else
+          resolve(children)
+      });
+    });
+  }
+}
+
 function argNormalization(args: { path: string, includedFile: string | null, chunkSize?: number | null, rpt?: typeof readPkgTree | null, http?: typeof fetch }): { chunkSize: number; rpt: typeof readPkgTree; http: typeof fetch; path: string; includedFile: string | null; } {
 
   return {
@@ -39,40 +52,32 @@ function argNormalization(args: { path: string, includedFile: string | null, chu
 export default async function noticeme(args: { path: string, includedFile: string | null, chunkSize?: number | null, rpt?: typeof readPkgTree | null, http?: typeof fetch }): Promise<string> {
   const { path, includedFile, chunkSize, rpt, http } = argNormalization(args);
 
- return new Promise((resolve, reject) => {
-    rpt(path, function (err:any, { children }:{children:PackageNode[]}) {
-      if (err) reject(err);
+  const children = await getPackages(rpt)(path)
 
-      const pkgJsonLicenses = new Map();
-      let coordinates = children.map(({ package:pkg, children: more }:PackageNode ) => {
-        children.push(...more);
-        const coordinate = npmjsCoordinates(pkg);
+  const pkgJsonLicenses = new Map();
+  let coordinates = children.map(({ package: pkg, children: more }: PackageNode) => {
+    children.push(...more);
+    const coordinate = npmjsCoordinates(pkg);
 
-        // TODO: make use of these as fallback
-        pkgJsonLicenses.set(coordinate, pkg.license);
+    // TODO: make use of these as fallback
+    pkgJsonLicenses.set(coordinate, pkg.license);
 
-        return coordinate;
-      });
-
-     
-      if (includedFile){
-        coordinates = coordinates.concat(retrieveIncludedJson(includedFile).
-            map((pkg) => npmjsCoordinates(pkg)));
-      };
-
-     
-      if (includedFile){
-        coordinates = coordinates.concat(retrieveIncludedJson(includedFile).
-            map((pkg) => npmjsCoordinates(pkg)));
-      }
-
-      const service = new NoticeService('https://api.clearlydefined.io/notices', http);
-      service.generateNotice(coordinates, chunkSize).then(json => 
-        resolve(json.content)
-      ).catch(error => {
-        reject(error);
-      })
-
-    });
+    return coordinate;
   });
+
+
+  if (includedFile) {
+    coordinates = coordinates.concat(retrieveIncludedJson(includedFile).
+      map((pkg) => npmjsCoordinates(pkg)));
+  };
+
+
+  if (includedFile) {
+    coordinates = coordinates.concat(retrieveIncludedJson(includedFile).
+      map((pkg) => npmjsCoordinates(pkg)));
+  }
+
+  const service = new NoticeService('https://api.clearlydefined.io/notices', http);
+  const notice = await service.generateNotice(coordinates, chunkSize)
+  return notice.content;
 }
